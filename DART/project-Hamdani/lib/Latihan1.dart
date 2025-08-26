6d38cd051d458ec93492cb276accdc4c9fdd16f9
Hamdani <h07352311110@gmail.com>
2025-08-26 11:57:24 +0900

    biodata

---
+++ DART/project-Hamdani/lib/Latihan1.dart
@@ -0,0 +1,20 @@
+void main() {
+  String name = "Hamdani";
+
+  int umur = 20;
+
+  double tinggi = 175.5;
+
+  num beratBadan = 55.3;
+
+  dynamic hobi = "menggambar";
+
+  bool isActive = false;
+
+  print("nama = $name");
+  print("umur = $umur");
+  print("tinggi = $tinggi");
+  print("beratBadan = $beratBadan");
+  print("hobi = $hobi");
+  print("Mahasiswa ini statusnya : $isActive");
+}
